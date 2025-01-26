e47b46706ccd23897ab90633058f63f1b339341a
Miriam FG <miriam.fuentes@tinet.cat>
2025-01-26 09:33:24 +0100

    Add motif search functionality to seqClass.sh

--- seqCLASS.sh
+++ seqCLASS.sh
@@ -24,3 +24,12 @@ else
   echo "No motif provided to search for"
 fi
 
+motif=$(echo $2 | tr a-z A-Z)
+if [[ -n $motif ]]; then
+  echo -en "Motif search enabled: looking for motif '$motif' in sequence '$seq'... "
+  if [[ $seq =~ $motif ]]; then
+    echo "FOUND"
+  else
+    echo "NOT FOUND"
+  fi
+fi
