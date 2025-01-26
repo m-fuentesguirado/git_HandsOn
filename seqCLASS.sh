734eb3a638ba2d084646990e2681956f795e97b8
Miriam FG <miriam.fuentes@tinet.cat>
2025-01-26 12:05:22 +0100

    Fix seqClass.sh to correctly classify RNA and DNA sequences

--- seqCLASS.sh
+++ seqCLASS.sh
@@ -1,36 +1,25 @@
+
+### Cleaned-up Code Example:
+
+```bash
 #!/bin/bash
-seq=$1
-seq=$(echo $seq | tr a-z A-Z)  # Note we just added this line
-if [[ $seq =~ ^[ACGTU]+$ ]]; then # if seq contains "T, it is DNA
-  if [[ $seq =~ T ]]; then
-    echo "The sequence is DNA"
-  elif [[ $seq =~ U ]]; then #IF SEQ CONTAINS U it;s RNA
-    echo "The sequence is RNA"
-  else
-    echo "The sequence can be DNA or RNA"
-  fi
-else
-  echo "The sequence is not DNA nor RNA"
-fi
-# Adding motif search functionality
-motif=$2
-if [[ -n $motif ]]; then
-  if [[ $seq == *$motif* ]]; then
+
+# Check if the sequence and motif are provided
+if [[ -n $1 && -n $2 ]]; then
+  seq=$1
+  motif=$(echo $2 | tr a-z A-Z)  # Convert motif to uppercase for case-insensitive search
+
+  echo -en "Motif search enabled: looking for motif '$motif' in sequence '$seq'..."
+
+  # Search for motif in sequence
+  if [[ $seq =~ $motif ]]; then
     echo "Motif '$motif' found in the sequence!"
   else
-    echo "Motif '$motif' not found in the sequence, idiot"
+    echo "MOTIF NOT FOUND IN SEQUENCE"
   fi
 else
-  echo "No motif provided to search for"
+  echo "No sequence or motif provided"
 fi
 
-motif=$(echo $2 | tr a-z A-Z)
-if [[ -n $motif ]]; then
-  echo -en "Motif search enabled: looking for motif '$motif' in sequence '$seq'... "
-  if [[ $seq =~ $motif ]]; then
-    echo "Motif is located in the sequence!"
-  else
-    echo "MOTIF NOT FOUND IN SEQUENCE"
-fi
-fi
-echo "FOUND"
+echo "Search complete"
+
