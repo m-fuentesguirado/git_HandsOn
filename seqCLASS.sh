06fd6a657dd161185b47a9147aa9562db930e133
Miriam FG <miriam.fuentes@tinet.cat>
2025-01-26 07:45:38 +0100

    Update seqCLASS.sh with input sequence case conversion

--- seqCLASS.sh
+++ seqCLASS.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 seq=$1
+seq=$(echo $seq | tr a-z A-Z)  # Note we just added this line
 if [[ $seq =~ ^[ACGTU]+$ ]]; then
   if [[ $seq =~ T ]]; then
     echo "The sequence is DNA"
@@ -11,3 +12,4 @@ if [[ $seq =~ ^[ACGTU]+$ ]]; then
 else
   echo "The sequence is not DNA nor RNA"
 fi
+
