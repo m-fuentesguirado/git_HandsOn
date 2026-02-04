33ec0dc47f809af818a52795fcd1d2aad12f516a
Miriam FG <miriam.fuentesguirado@gmail.com>
2026-02-04 10:38:55 +0800

    Changes in case

--- seqClass.py
+++ seqClass.py
@@ -12,6 +12,7 @@ if len(sys.argv) == 1:
 
 args = parser.parse_args()
 
+args.seq = args.seq.upper()                 # New line for case 
 if re.search('^[ACGTU]+$', args.seq):
     if re.search('T', args.seq):
         print ('The sequence is DNA')
@@ -21,4 +22,3 @@ if re.search('^[ACGTU]+$', args.seq):
         print ('The sequence can be DNA or RNA')
 else:
     print ('The sequence is not DNA nor RNA')
-
